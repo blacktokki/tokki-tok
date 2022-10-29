93a476604d7526b735ea449d0c913632fb590175
blacktokki <blacktokki@github.com>
2022-10-29 16:38:17 +0000

    initial backend

---
+++ scripts/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Usage:
+# bash scripts/setup.sh param1 param2 param3
+# * param1: github username
+# * param2: database username
+# * param3: database password
+
+PROPERTY_FILE=./.env
+FRONTEND_DIRECTORY=./frontend
+
+git config user.email "$1@github.com"
+git config user.name "$1"
+git config credential.helper store
+
+sudo apt-get update
+sudo apt-get --assume-yes install python3
+sudo apt-get --assume-yes install python3-pip
+sudo apt-get --assume-yes install python3-venv
+sudo apt-get --assume-yes install libmysqlclient-dev
+python3 -m venv ../venv1
+. ../venv1/bin/activate
+pip3 install -r requirements.txt
+
+sudo mysql -u root -p" " -e"
+create user '$2'@'%' identified by '$3';
+GRANT ALL PRIVILEGES ON *.* TO '$2'@'%';
+flush privileges;
+"
+
+sudo mysql -u root -p" " -e"
+SET character_set_server = 'utf8';
+create database db1_messenger;
+"
+
+echo "DATABASE_HOST=127.0.0.1" >> $PROPERTY_FILE
+echo "DATABASE_USER=$2" >> $PROPERTY_FILE
+echo "DATABASE_PASS=$3" >> $PROPERTY_FILE
