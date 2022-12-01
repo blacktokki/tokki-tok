6bef60d2746efbe8b39233b26c03403aa8a94853
blacktokki <blacktokki@github.com>
2022-12-01 18:58:23 +0900

    webSocketContext 수정

--- frontend/src/hooks/useWebsocketContext.tsx
+++ frontend/src/hooks/useWebsocketContext.tsx
@@ -4,13 +4,19 @@ import { SendJsonMessage } from "react-use-websocket/dist/lib/types"
 
 const WebSocketContext = createContext<{lastJsonMessage:any, sendJsonMessage:SendJsonMessage }>({lastJsonMessage:null, sendJsonMessage:()=>{}});
 
-export const WebSocketProvider = ({disable, children}:{disable?:boolean, children:React.ReactNode})=>{
+const WebSocketProviderInternal = ({children}:{children:React.ReactNode})=>{
   const { lastJsonMessage, sendJsonMessage } = useWebSocket('ws://localhost:9000/messenger/ws/',{
     shouldReconnect: (closeEvent) => true
   })
-  return disable?<>{children}</>:<WebSocketContext.Provider value={{lastJsonMessage, sendJsonMessage}}>
+  return <WebSocketContext.Provider value={{lastJsonMessage, sendJsonMessage}}>
+  {children}
+</WebSocketContext.Provider>
+}
+
+export const WebSocketProvider = ({disable, children}:{disable?:boolean, children:React.ReactNode})=>{
+  return disable?<>{children}</>:<WebSocketProviderInternal>
     {children}
-  </WebSocketContext.Provider>
+  </WebSocketProviderInternal>
 }
 
 export default ()=>{
