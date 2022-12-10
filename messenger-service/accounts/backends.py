7b0e22cb5ae746cdf2b2c613b7a5350ea2fb3a49
blacktokki <blacktokki@github.com>
2022-12-10 09:32:09 +0000

    backends.py 수정

--- messenger-service/accounts/backends.py
+++ messenger-service/accounts/backends.py
@@ -14,7 +14,14 @@ class AuthBackend(BaseBackend):
         url = f"http://{settings.TASK_SERVICE_DOMAIN}"
         csrf = sessions.get(f"{url}/api/v1/user/csrf/").content
         mp_encoder = MultipartEncoder(fields={'username': username, 'password': password})
-        headers = {"X-CSRF-TOKEN": csrf, "Content-Type": mp_encoder.content_type}
+        # remote_addr = request.META.get('REMOTE_ADDR')
+        headers = {
+            "X-FORWARDED-FOR": request.META.get('HTTP_X_FORWARDED_FOR'),
+            "X-REAL-IP": request.META.get('HTTP_X_REAL_IP'),
+            "User-Agent": request.META.get('HTTP_USER_AGENT'),
+            "X-CSRF-TOKEN": csrf, 
+            "Content-Type": mp_encoder.content_type
+        }
         res = sessions.post(f"{url}/login", headers=headers, data=mp_encoder)
         if res.ok and res.status_code == 200 and len(res.history) == 0:
             res = sessions.get(f"{url}/api/v1/user/?self=true")
