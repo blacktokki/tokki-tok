294dd016a8b0447af1b47776a069a6002f0d79f8
blacktokki <39031723+blacktokki@users.noreply.github.com>
2023-02-11 23:39:26 +0900

    리프레시 토큰 개선

--- frontend/src/apis/axios.ts
+++ frontend/src/apis/axios.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 // @ts-ignore
 import {API_URL} from "@env"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const baseURL =  `${API_URL}/messenger/`
 const defaultOption:AxiosRequestConfig = {
     baseURL,
@@ -26,11 +27,20 @@ _axios.interceptors.response.use(
         return response;
     },
     error => {
-        // if (error.response.status === 401) {
-        //     // redirect login
-        // }
+        if (error.response.status === 401) {
+            AsyncStorage.getItem("Authorization").then(refreshToken)
+        }
         return Promise.reject(error)
     }
 )
+
+export const refreshToken = async(token:string|null)=>{
+    const r = await _axios.post("/api-token-refresh/", {token}, {headers:{'Authorization':''}})
+    if (r.status == 200 && r.data !== ''){
+        await AsyncStorage.setItem("Authorization", r.data)
+        return r.data
+    }
+    return ''
+}
 
 export default _axios
