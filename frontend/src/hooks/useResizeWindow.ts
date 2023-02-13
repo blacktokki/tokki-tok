f8404b76f214e79eb513f8a2ac371b01ea39fe27
blacktokki <39031723+blacktokki@users.noreply.github.com>
2023-02-13 23:15:50 +0900

    keyboard 대응1

--- frontend/src/hooks/useResizeWindow.ts
+++ frontend/src/hooks/useResizeWindow.ts
@@ -1,16 +1,26 @@
 import { useState, useEffect } from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, Keyboard } from 'react-native';
 
 type WindowType = 'portrait' | 'landscape'
 
 export default function useResizeWindow() {
   const [window, setWindow] = useState(Dimensions.get('window'));
+  const [keyboard, setKeyboard] = useState(false)
   const onChange = () => setWindow(Dimensions.get('window'))
+  const onKeyboardShow = ()=>setKeyboard(true)
+  const onKeyboardHide = ()=>setKeyboard(false)
+
   useEffect(() => {
+    Keyboard.addListener("keyboardDidShow", onKeyboardShow)
+    Keyboard.addListener("keyboardDidHide", onKeyboardHide)
     Dimensions.addEventListener("change", onChange);
     return () => {
+      Keyboard.removeListener("keyboardDidShow", onKeyboardShow)
+      Keyboard.removeListener("keyboardDidHide", onKeyboardHide)
       Dimensions.removeEventListener("change", onChange);
     };
   });
+  if (keyboard)
+    return 'portrait'
   return window.height >= window.width?'portrait':'landscape' as WindowType;
 }
