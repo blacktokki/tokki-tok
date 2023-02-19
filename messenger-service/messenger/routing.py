5fa849b2cba6f307d1180de0df569ecdd65a7d9a
blacktokki <blacktokki@github.com>
2023-02-19 06:28:41 +0000

    webrtc기능 2

--- messenger-service/messenger/routing.py
+++ messenger-service/messenger/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers, rtc
 
 websocket_urlpatterns = [
-    re_path(r'ws/', consumers.MessengerConsumer.as_asgi()),
     re_path(r'ws/rtc/', rtc.RtcConsumer.as_asgi()),
+    re_path(r'ws/', consumers.MessengerConsumer.as_asgi()),
 ]
--- messenger-service/messenger/rtc.py
+++ messenger-service/messenger/rtc.py
@@ -1,4 +1,5 @@
 import json
+from django.contrib.auth import get_user_model
 from channels.generic.websocket import WebsocketConsumer
 from messenger.consumers import connect, disconnect, USER_PREFIX
 from asgiref.sync import async_to_sync
@@ -15,17 +16,17 @@ class RtcConsumer(WebsocketConsumer):
     # Receive message from client WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        user = text_data_json['data']['user']
-        message_type = text_data_json['data']['message_type']
-        message = text_data_json['data']['message']
-        print(self.user, "send to", user)
+        data = text_data_json['data']
+        message_type = text_data_json['type']
+        if 'username' in text_data_json:
+            user = get_user_model().objects.get_by_natural_key(text_data_json['username']).id
+        else:
+            user = text_data_json['user']
+        print(self.user.id, "send", message_type, "to", user)
         async_to_sync(self.channel_layer.group_send)(f"{USER_PREFIX}{user}", {
             'type': 'send_message',
-            'data': {'sender': self.user.id, 'message_type': message_type, 'message': message}
+            'message': {'sender': self.user.id, 'type':message_type, 'data': data}
         })
 
     def send_message(self, event):
-        self.send(text_data=json.dumps({
-            'type': event['data']['message_type'],
-            'data': {'sender': event['data']['sender'], 'message': event['data']['message']}
-        }))
+        self.send(text_data=json.dumps(event['message']))
