507f5ee4e00339389f401c54581d9ab8d8888dba
blacktokki <blacktokki@github.com>
2023-05-03 18:48:20 +0900

    Revert "클라이언트 프록시 모드 구현" This reverts commit a34e28e85cdc253cbd3fc8743d96ce619e7d704c.

--- frontend/src/apis/proxy.ts
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-import {API_URL as API_URL_ORIGIN, USE_PROXY as USE_PROXY} from "@env"
-if (USE_PROXY){
-    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/proxy-sw.js?api_url=${API_URL_ORIGIN}`)
-}
-export const API_URL = USE_PROXY?"":API_URL_ORIGIN
