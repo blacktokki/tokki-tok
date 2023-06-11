534066b957651cba57848a3c80154365e8161f30
blacktokki <39031723+blacktokki@users.noreply.github.com>
2023-06-11 19:43:43 +0900

    HomeScreen 분리

---
+++ frontend/src/screens/main/HomeScreen/ConfigSections.tsx
@@ -0,0 +1,49 @@
+import CommonSection from "../../../components/CommonSection"
+import Colors from "../../../constants/Colors"
+import useColorScheme, { setColorScheme } from "../../../hooks/useColorScheme"
+import { StyleSheet, Text, View} from 'react-native';
+import useLangContext from "../../../hooks/useLangContext";
+import { useColorScheme as useConfigColorScheme} from 'react-native-appearance';
+import useAuthContext from "../../../hooks/useAuthContext";
+import useFirebaseContext from "../../../hooks/useFirebaseContext";
+import TextButton from "../../../components/TextButton";
+
+
+const ConfigSection = ({title, children}:{title:string, children?: React.ReactNode})=>{
+  const theme = useColorScheme()
+  const color = Colors[theme].text
+  return <CommonSection bodyStyle={{alignItems:'flex-start', backgroundColor:theme=='light'?'transparent':"black"}}>
+    <Text style={{fontSize:20, color, fontWeight:'600'}}>{title}</Text>
+    {children}
+  </CommonSection>
+}
+
+export default ()=>{
+  const { lang, locale, setLocale } = useLangContext()
+  const theme = useColorScheme()
+  const configTheme = useConfigColorScheme()
+  const { auth } = useAuthContext()
+  const {enable:noti, setEnable:setNoti} = useFirebaseContext(auth)
+  const color = Colors[theme].text
+  return <>
+    <ConfigSection title={lang('* Notification Settings')}>
+      <View style={{flexDirection:'row'}}>
+        {[[lang('On'), true], [lang('Off'), false]].map(([title, n])=><TextButton
+          key={title} title={title || ''} textStyle={{fontSize:16, color, textDecorationLine:noti==n?'underline':'none'}} style={{borderRadius:20}} onPress={()=>{setNoti(n)}}/>)}
+      </View>
+    </ConfigSection>
+    <ConfigSection title={lang('* Language Settings')}>
+    <View style={{flexDirection:'row'}}>
+        {[[lang('Auto'), 'auto'], ['한국어', 'ko'], ['English', 'en']].map(([title, l])=><TextButton 
+          key={title} title={title || ''} textStyle={{fontSize:16, color, textDecorationLine:locale==l?'underline':'none'}} style={{borderRadius:20}} onPress={()=>setLocale(l)}/>)}
+      </View>
+    </ConfigSection>
+    <ConfigSection title={lang('* Skin Settings')}>
+      <View style={{flexDirection:'row'}}>
+        {[[lang('Auto'), 'no-preference'], [lang('Light'), 'light'], [lang('Dark'), 'dark']].map(([title, colorScheme])=><TextButton 
+          key={title} title={title} textStyle={{fontSize:16, color, textDecorationLine:configTheme==colorScheme?'underline':'none'}} style={{borderRadius:20}} onPress={(
+            )=>setColorScheme(colorScheme)}/>)}
+      </View>
+    </ConfigSection>
+  </>
+}
--- frontend/src/screens/main/HomeScreen/index.tsx
+++ frontend/src/screens/main/HomeScreen/index.tsx
@@ -2,29 +2,28 @@ import React, {useState, useEffect, useLayoutEffect} from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
 import { StyleSheet, Text, View} from 'react-native';
 import { Text as StyledText } from 'react-native-paper';
-import { useColorScheme as useConfigColorScheme} from 'react-native-appearance';
-import useResizeContext from '../../hooks/useResizeContext';
-import TabView from '../../components/TabView';
-import HeaderRight from '../../components/HeaderRight';
-import TextButton from '../../components/TextButton';
-import Colors from '../../constants/Colors';
-import useColorScheme, { setColorScheme } from '../../hooks/useColorScheme';
-import useModalsContext from '../../hooks/useModalsContext';
-import ChannelEditModal from '../../modals/ChannelEditModal';
-import useLangContext from '../../hooks/useLangContext';
-import ContractFooter from '../../components/ContractFooter';
-import { TabViewRecord } from '../../types';
-import useAuthContext from '../../hooks/useAuthContext';
-import useUserMembershipList from '../../hooks/lists/useUserMembershipList';
+import useResizeContext from '../../../hooks/useResizeContext';
+import TabView from '../../../components/TabView';
+import HeaderRight from '../../../components/HeaderRight';
+import TextButton from '../../../components/TextButton';
+import Colors from '../../../constants/Colors';
+import useColorScheme  from '../../../hooks/useColorScheme';
+import useModalsContext from '../../../hooks/useModalsContext';
+import ChannelEditModal from '../../../modals/ChannelEditModal';
+import useLangContext from '../../../hooks/useLangContext';
+import ContractFooter from '../../../components/ContractFooter';
+import { TabViewRecord } from '../../../types';
+import useAuthContext from '../../../hooks/useAuthContext';
+import useUserMembershipList from '../../../hooks/lists/useUserMembershipList';
 import { FontAwesome, Ionicons, MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons';
-import { useMessengerChannelSorted } from '../../hooks/lists/useMessengerChannelList';
+import { useMessengerChannelSorted } from '../../../hooks/lists/useMessengerChannelList';
 import { ScrollView } from 'react-native-gesture-handler';
-import CommonItem from '../../components/CommonItem';
-import MemberItem from '../../components/MemberItem';
-import ProfileModal from '../../modals/ProfileModal';
-import { navigate } from '../../navigation';
-import CommonSection from '../../components/CommonSection';
-import useFirebaseContext from '../../hooks/useFirebaseContext';
+import CommonItem from '../../../components/CommonItem';
+import MemberItem from '../../../components/MemberItem';
+import ProfileModal from '../../../modals/ProfileModal';
+import { navigate } from '../../../navigation';
+import CommonSection from '../../../components/CommonSection';
+import ConfigSections from './ConfigSections';
 
 const MemberTabView = ()=>{
   const {auth} = useAuthContext()
@@ -64,44 +63,6 @@ const MessengerTabView = ()=>{
   </ScrollView>
 }
 
-const ConfigSection = ({title, children}:{title:string, children?: React.ReactNode})=>{
-  const theme = useColorScheme()
-  const color = Colors[theme].text
-  return <CommonSection bodyStyle={{alignItems:'flex-start', backgroundColor:theme=='light'?'transparent':"black"}}>
-    <Text style={{fontSize:20, color, fontWeight:'600'}}>{title}</Text>
-    {children}
-  </CommonSection>
-}
-
-const ConfigSections = ()=>{
-  const { lang, locale, setLocale } = useLangContext()
-  const theme = useColorScheme()
-  const configTheme = useConfigColorScheme()
-  const { auth } = useAuthContext()
-  const {enable:noti, setEnable:setNoti} = useFirebaseContext(auth)
-  const color = Colors[theme].text
-  return <>
-    <ConfigSection title={lang('* Notification Settings')}>
-      <View style={{flexDirection:'row'}}>
-        {[[lang('On'), true], [lang('Off'), false]].map(([title, n])=><TextButton 
-          key={title} title={title || ''} textStyle={{fontSize:16, color, textDecorationLine:noti==n?'underline':'none'}} style={{borderRadius:20}} onPress={()=>{setNoti(n)}}/>)}
-      </View>
-    </ConfigSection>
-    <ConfigSection title={lang('* Language Settings')}>
-    <View style={{flexDirection:'row'}}>
-        {[[lang('Auto'), 'auto'], ['한국어', 'ko'], ['English', 'en']].map(([title, l])=><TextButton 
-          key={title} title={title || ''} textStyle={{fontSize:16, color, textDecorationLine:locale==l?'underline':'none'}} style={{borderRadius:20}} onPress={()=>setLocale(l)}/>)}
-      </View>
-    </ConfigSection>
-    <ConfigSection title={lang('* Skin Settings')}>
-      <View style={{flexDirection:'row'}}>
-        {[[lang('Auto'), 'no-preference'], [lang('Light'), 'light'], [lang('Dark'), 'dark']].map(([title, colorScheme])=><TextButton 
-          key={title} title={title} textStyle={{fontSize:16, color, textDecorationLine:configTheme==colorScheme?'underline':'none'}} style={{borderRadius:20}} onPress={(
-            )=>setColorScheme(colorScheme)}/>)}
-      </View>
-    </ConfigSection>
-  </>
-}
 
 const ConfigTabView = ()=>{
   return <View style={{flex:1, backgroundColor:'white'}}>
