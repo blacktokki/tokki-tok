18a0acaa88fc9871898f35b9343c57ecc219653f
blacktokki <blacktokki@github.com>
2023-06-21 19:03:09 +0900

    test : bot 스크립트 수정

--- frontend/scripts/bot.js
+++ frontend/scripts/bot.js
@@ -3,8 +3,11 @@
 const axios = require('axios')
 const WebSocket = require('ws');
 require('dotenv').config()
-const USER_NUMBER = 5
-const channel = 3
+
+const USER_NUMBER = parseInt(process.env.TEST_USER_NUMBER)
+const MESSAGE_NUMBER = parseInt(process.env.TEST_MESSAGE_NUMBER)
+const channel = parseInt(process.env.TEST_CHANNEL_ID)
+
 const [SCHEMA, DOMAIN] = `${process.env.API_URL}`.split('://')
 const endpoint = `${process.env.API_URL}/messenger`
 const createUser = async()=>{
@@ -20,11 +23,11 @@ const createUser = async()=>{
     const contents = [].concat(...(await axios.get(`https://baconipsum.com/api/?type=all-meat&paras=20`)).data.map(v=>v.split('  ')))
     const users = await Promise.all(Array(USER_NUMBER).fill().map(createUser))
     await axios.post(`${endpoint}/api/v1/messengermembers/bulk/`, {channel, user_ids:users.map(user=>user.user.id)}, users[0].options)
-    await users.map(async user=>await axios.post(`${endpoint}/api/v1/messengercontents/messages/`, {
+    await Array(MESSAGE_NUMBER).fill(users).map(async users=>await Promise.all(users.map(async user=>await axios.post(`${endpoint}/api/v1/messengercontents/messages/`, {
         channel,
         content: contents[user.user.id % contents.length],
         user: user.user.id
-    }, user.options))
+    }, user.options))))
     const userToMember = {}
     const members = (await axios.get(`${endpoint}/api/v1/messengermembers/?channel=${channel}`, users[0].options)).data.forEach(v=>{userToMember[v.user]=v.id})
     await Promise.all(users.map(user=>axios.delete(`${endpoint}/api/v1/messengermembers/${userToMember[user.user.id]}/`, user.options)))
