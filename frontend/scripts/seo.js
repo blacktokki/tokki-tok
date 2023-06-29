7108802bcec01ba845f1d690c6aa732513748184
blacktokki <blacktokki@github.com>
2023-06-29 18:56:51 +0900

    feat : 이미지 Asset 교체

--- frontend/scripts/seo.js
+++ frontend/scripts/seo.js
@@ -1,5 +1,5 @@
 module.exports = {
     files: 'web-build/index.html',
     from: '<head>',
-    to: '<head><meta property="og:image" content="/tokki-tok/preview.png"/>',
+    to: '<head><meta property="og:image" content="/tokki-tok/pwa/chrome-icon/chrome-icon-512.png"/>',
 };
