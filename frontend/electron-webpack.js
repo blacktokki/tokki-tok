05dbaa96fffa75bd5581398c5cf488c7a117408c
blacktokki <39031723+blacktokki@users.noreply.github.com>
2024-03-19 22:03:20 +0900

    fix : electron 대응1

--- frontend/electron-webpack.js
+++ frontend/electron-webpack.js
@@ -4,6 +4,7 @@ module.exports = withExpoAdapter({
   projectRoot: __dirname,
   "main": {
     "sourceDirectory": "src/electron",
-  }
+  },
+  whiteListedModules: ['react-query', 'react-use-websocket']
   // Provide any overrides for electron-webpack: https://github.com/electron-userland/electron-webpack/blob/master/docs/en/configuration.md
 });
